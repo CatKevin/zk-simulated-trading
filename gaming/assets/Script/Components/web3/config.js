4739708babe5b9293251fdf215c32aba2693e290
CatKevin <catmi9944@gmail.com>
2024-04-29 00:05:20 +0800

    feat(contract): deploy Verify.sol on sepolia network and verify it on scrollscan

--- gaming/assets/Script/Components/web3/config.js
+++ gaming/assets/Script/Components/web3/config.js
@@ -1,3 +1,5 @@
 
 export const  BEARER_TOKEN = "your_sindri_api_key";
 export const  CIRCUIT_ID = "53c9ed41-50f1-4d47-a306-705f50d8262d";
+
+export const  VERIFY_CONTRACT_ADDRESS = "0x497B05AC1F135B390d5Ac0c2dDb06C5a915Af46F";
