dbf3ebacb1780f42ca2914a846fe94208427ee4b
CatKevin <catmi666@163.com>
2024-04-30 05:19:57 +0800

    feat(release): release version 2.0.0

--- release/web-mobile/main.b0c71.js
+++ release/web-mobile/main.b0c71.js
@@ -151,7 +151,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.03772.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.96429.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -187,7 +187,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.46a5e.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.b0c71.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -214,12 +214,12 @@ else if (window.jsb) {
 
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.b500c.js');
+        require('src/settings.01e67.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.b500c.js');
+        require('src/settings.01e67.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
