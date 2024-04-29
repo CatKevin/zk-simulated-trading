47910bb1f73acb4b03c046c556d10502bc6777e7
CatKevin <catmi666@163.com>
2024-04-30 02:27:06 +0800

    feat(release): release version 1.1.0

--- release/web-mobile/main.46a5e.js
+++ release/web-mobile/main.46a5e.js
@@ -187,7 +187,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.d7e5a.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.46a5e.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -214,12 +214,12 @@ else if (window.jsb) {
 
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.f270d.js');
+        require('src/settings.b500c.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.f270d.js');
+        require('src/settings.b500c.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
